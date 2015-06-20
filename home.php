2045040b1bf65aab3c828ae8cdba1786b072bc7b
Maya Balaji <metallicmay@gmail.com>
2015-06-20 15:00:55 +0530

    Update home.php

--- home.php
+++ home.php
@@ -3,28 +3,12 @@
  ?> 
 <html>
 <title> HOMEPAGE </title>
-<style type='text/css'>
-#logout
-{ position:absolute;
-  right:50px;  
-  top:0px;
-  }
-  
-#ticker
-{ position:absolute;
-  right:50px;  
-  top:50px;
-  outline:1px solid black;
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
 
-}
-table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-}
-</style>
 <?php
 
-/* INCLUSION OF LIBRARY FILEs*/
+    /* INCLUSION OF LIBRARY FILEs*/
 	require_once( 'lib/Facebook/FacebookSession.php');
 	require_once( 'lib/Facebook/FacebookRequest.php' );
 	require_once( 'lib/Facebook/FacebookResponse.php' );
@@ -56,6 +40,7 @@ table, th, td {
 	use Facebook\FacebookCurl;
 /*PROCESS*/
 	
+	
 	//1.Start Session
 	 session_start();
 	  //check if users wants to logout
@@ -77,12 +62,13 @@ table, th, td {
 	}
 	//logout
 	$logout = 'http://localhost:8080/dsg/index.php?logout=true';
+	
 	if(isset($sess)){
 		//store the token in the php session
-	 		$_SESSION['fb_token']=$sess->getToken();
-	 		//create request object,execute and capture response
-			 $sess = new FacebookSession($sess->getToken());
-	         // create a session using saved token or the new one we generated at login
+	 	$_SESSION['fb_token']=$sess->getToken();
+	 	//create request object,execute and capture response
+		$sess = new FacebookSession($sess->getToken());
+	    // create a session using saved token or the new one we generated at login
 		$request = new FacebookRequest($sess, 'GET', '/me');
 		// from response get graph object
 		$response = $request->execute();
@@ -90,40 +76,39 @@ table, th, td {
 		// use graph object methods to get user details
 		$name= $graph->getName();
 		$id = $graph->getId();
-		 //select user from database
-		$query= mysql_query("SELECT * FROM users WHERE NAME ='".$name."'") or die(mysql_error());
+				
+		//select user from database
+		$query= mysql_query("SELECT * FROM users WHERE NAME ='".$name."'") or die(mysql_error());  //user id is unique
         $result=mysql_fetch_array($query);
-		//fetch liquid cash value
+		//fetch liquid cash value and user id from user table
 		$liqcash=$result['LIQUIDCASH'];
-		$ex_stock=$result['STOCKS'];
+		$uid = $result['UID'];
 		$liqnew="";
-		$tot="";
-		$stocks = "";
+		
 		echo "Hi $name";
 		echo "<div align=right><a href='".$logout."'>Logout</a></div>";
 		echo"<div align=center><a href=home.php> Homepage &nbsp;</a> <a href=user.php> Userpage &nbsp;</a> <a href=lb.php> &nbsp;  Leaderboard  </a></div>";
 		echo "<div align=center>Liquid Cash: $liqcash <br></div>";
 		echo "<div align=center><br>HOMEPAGE</div>";
-	     //calculate new values after buy/sell button is clicked and update in database
-		if (isset($_GET['prices'])) 
+	    
+		//calculate new values after buy/sell button is clicked and update in database
+		if (isset($_GET['liq'])) 
 		 {
-		 $prices = explode(",", $_GET["prices"]);
-		 $liqnew = $prices[0];
-		 $stocks = $prices[1];
-		 $tot=$liqnew+$stocks+$ex_stock;
+		 $liqnew = $_GET['liq'];
 		 $check=true;
-		 }
-		if($check)
+		  }
+		 if($check)
 		{
 		
 		$sql="UPDATE users 
-		      SET LIQUIDCASH=$liqnew, STOCKS = STOCKS + $stocks, TOTAL=$tot
+		      SET LIQUIDCASH=$liqnew
 		      WHERE name='".$name."'";
 		$quer=mysql_query($sql);
         if(!$quer)
          echo "Failed" .mysql_error();
 		 $check = false;
 		 }
+		
 		}
 		else{
 		//else echo login
@@ -134,6 +119,7 @@ table, th, td {
 		 
 		$objYahooStock = new YahooStock; //creating object
 		$objYahooStock->addFormat("sl1hg"); //adding format/parameters to be fetched
+		
         //adding company stock code to be fetched
 		$objYahooStock->addStock("msft"); 
 		$objYahooStock->addStock("yhoo");
@@ -141,37 +127,89 @@ table, th, td {
 		$objYahooStock->addStock("aapl"); 
 
 	?>	
-	<script type="text/javascript">
-        function buy(price)
+
+<script type="text/javascript">
+function buy(sym,price)
         { 
 		 var qty; 
 		 do
 		 {
 		 qty = prompt("Enter quantity: ", "100");
 		 } while(isNaN(qty));
-         var liq = "<?php echo $liqcash;?>";
+		 
+         var liq = '<?php echo $liqcash?>';
+		 var uid = '<?php echo $uid?>';
 		 var fin;
 		 var tprice;
 		 tprice = price*qty;
+		 if(tprice>liq)
+		 { alert('Cannot buy');
+		 }
+		 else
+		 {
 		 fin=liq-tprice;
-		 window.location = "?prices=" + fin + "," + tprice;
+		 window.location = "?liq=" + fin;
+		 var xmlhttp;
+		if (window.XMLHttpRequest)
+		  {// code for IE7+, Firefox, Chrome, Opera, Safari
+		  xmlhttp=new XMLHttpRequest();
+		  }
+		else
+		  {// code for IE6, IE5
+		  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		  }
+		xmlhttp.onreadystatechange=function()
+		  {
+		  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+			
+			}
+		  }
+		  xmlhttp.open("GET","insert.php?uid=" + uid + "&sym=" + sym + "&qty=" + qty + "&price=" + price,true);
+          xmlhttp.send();
+		 }
         }
-        function sell(price)
+   
+	function sell(sym,price)
          {
 		 var qty; 
 		 do
 		 {
 		 qty = prompt("Enter quantity: ", "100");
 		 } while(isNaN(qty));
-         var liq = "<?php echo $liqcash;?>";
-		 var fin;
-		 var tprice;
-		 tprice = (-price)*qty;
-		 fin=liq-tprice;
-		 window.location = "?prices=" + fin + "," + tprice;
-         }
-		 
-		 function reset()
+         	var liq = '<?php echo $liqcash?>';
+		 var uid = '<?php echo $uid?>';
+		 var fin;		 
+		 var xmlhttp;
+		if (window.XMLHttpRequest)
+		  {// code for IE7+, Firefox, Chrome, Opera, Safari
+		  xmlhttp=new XMLHttpRequest();
+		  }
+		else
+		  {// code for IE6, IE5
+		  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		  }
+		  xmlhttp.onreadystatechange=function()
+		  {
+		  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+			
+			}
+		  }
+		  xmlhttp.open("GET","sell.php?sym=" + sym + "&qty=" + qty + "&price=" + price,false);
+          xmlhttp.send();
+		  if(xmlhttp.responseText.trim()=="No") {
+		   alert('User does not own this stock');
+		   } 
+		   else if(xmlhttp.responseText.trim()=="NoQ") {
+		  alert('Quantity too large');
+		  }
+		  else if(xmlhttp.responseText.trim()=="Yes") {
+		  fin=liq+tprice;
+		  window.location = "?liq=" + fin;
+		  }
+         }    		 
+	 function reset()
 		 {
 		 var xmlhttp;
 				if (window.XMLHttpRequest)
@@ -192,9 +230,9 @@ table, th, td {
 				xmlhttp.open("POST","reset.php",true);
 				xmlhttp.send();
 		 }
-	</script>
-  
-	<body>
+</script>
+	</head>
+  	<body>
 	   <form method="post">
 	    <table>            
             <tr>
@@ -213,8 +251,8 @@ table, th, td {
 			<td> <?php echo $stock[1]; ?> </td>
 			<td> <?php echo $stock[2]; ?> </td>
 			<td> <?php echo $stock[3]; ?> </td> 
-			<td><input type="button" value="Buy" onclick="buy('<?php echo $stock[1]?>')"></td>
-			<td><input type="button" value="Sell" onclick="sell('<?php echo $stock[1]?>')"> <br /> </td>
+			<td><input type="button" value="Buy" onclick="buy('<?php echo str_replace("\"","", $stock[0]); ?>','<?php echo $stock[1]; ?>')"></td>
+			<td><input type="button" value="Sell" onclick="sell('<?php echo str_replace("\"","", $stock[0]); ?>','<?php echo $stock[1];?>')"> <br /> </td>
           </tr>         
         <?php
 			}
